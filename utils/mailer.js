c8cbc24a46715bfe2a0af1836242127ff9645f26
Kevin Antony <kevinantonyofficial@gmail.com>
2025-09-26 18:54:56 +0530

    c

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,7 +1,7 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  service: 'outlook',
+  service: 'gmail',
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD
