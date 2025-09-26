09ab96c795210172422a85f52582e2a39bad26d4
Kevin Antony <kevinantonyofficial@gmail.com>
2025-09-26 15:15:34 +0530

    c

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,11 +1,13 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true, // use SSL
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASSWORD
-  }
+    pass: process.env.EMAIL_PASSWORD,
+  },
 });
 
 function sendOTPEmail(email, otp, name = '') {
